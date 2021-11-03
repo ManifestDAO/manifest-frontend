644502c0622757e7de636be54e7f48ffa449c493
Ivo <39407429+BoomerIvo@users.noreply.github.com>
2021-11-03 18:10:07 +0100

    Update dark.js

--- src/themes/dark.js
+++ src/themes/dark.js
@@ -13,12 +13,12 @@ const darkTheme = {
   backgroundColor: "rgba(44, 7, 69, 1)",
   background: `
     linear-gradient(180deg, rgba(8, 15, 53, 0), rgba(0, 0, 10, 0.9)),
-    linear-gradient(333deg, rgba(153, 207, 255, 0.2), rgba(180, 255, 217, 0.08)),
-    radial-gradient(circle at 77% 89%, rgba(125, 163, 169, 0.8), rgba(125, 163, 169, 0) 50%),
+    linear-gradient(333deg, rgba(44, 7, 69, 1), rgba(180, 255, 217, 0.08)),
+    radial-gradient(circle at 77% 89%, rgba(125, 163, 169, 0.8), rgba(44, 7, 69, 1) 50%),
     radial-gradient(circle at 15% 95%, rgba(125, 163, 169, 0.8), rgba(125, 163, 169, 0) 43%),
-    radial-gradient(circle at 65% 23%, rgba(137, 151, 119, 0.4), rgba(137, 151, 119, 0) 70%),
+    radial-gradient(circle at 65% 23%, rgba(137, 151, 119, 0.4), rgba(44, 7, 69, 1) 70%),
     radial-gradient(circle at 10% 0%, rgba(187, 211, 204, 0.33), rgba(187,211,204,0) 35%),
-    radial-gradient(circle at 11% 100%, rgba(131, 165, 203, 0.3), rgba(131, 165, 203, 0) 30%)
+    radial-gradient(circle at 11% 100%, rgba(131, 165, 203, 0.3), rgba(44, 7, 69, 1) 30%)
     `,
   paperBg: "rgba(54, 56, 64, 0.4)",
   modalBg: "#24242699",
