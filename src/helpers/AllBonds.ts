a6f3ca6c023b2231cd29b776c288d7d47de13d67
Ivo <39407429+BoomerIvo@users.noreply.github.com>
2021-11-11 18:37:04 +0100

    Update AllBonds.ts

--- src/helpers/AllBonds.ts
+++ src/helpers/AllBonds.ts
@@ -115,6 +115,7 @@ export const ohm_weth = new CustomBond({
 // Is it a stableCoin bond? use `new StableBond`
 // Is it an LP Bond? use `new LPBond`
 // Add new bonds to this array!!
+/*
 export const allBonds = [dai, frax, eth, ohm_dai, ohm_frax, lusd, ohm_lusd, ohm_weth];
 export const allBondsMap = allBonds.reduce((prevVal, bond) => {
   return { ...prevVal, [bond.name]: bond };
@@ -123,3 +124,4 @@ export const allBondsMap = allBonds.reduce((prevVal, bond) => {
 // Debug Log
 // console.log(allBondsMap);
 export default allBonds;
+*/
