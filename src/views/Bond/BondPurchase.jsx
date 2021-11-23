0b7a04326e89690db91255eeebb57bcc8d8798c3
Girth Brooks <girth.eth@gmail.com>
2021-11-23 11:38:57 -0500

    fixed debt ratio

--- src/views/Bond/BondPurchase.jsx
+++ src/views/Bond/BondPurchase.jsx
@@ -237,9 +237,7 @@ function BondPurchase({ bond, slippage, recipientAddress }) {
 
           <div className="data-row">
             <Typography>Debt Ratio</Typography>
-            <Typography>
-              {isBondLoading ? <Skeleton width="100px" /> : `${trim(bond.debtRatio / 10000000, 2)}%`}
-            </Typography>
+            <Typography>{isBondLoading ? <Skeleton width="100px" /> : `${trim(bond.debtRatio * 100, 2)}%`}</Typography>
           </div>
 
           <div className="data-row">
