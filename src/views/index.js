0e6a21764c1dde5b5d24c96f860d1035556eb662
heyaltamira <galgadot8585@gmail.com>
2022-03-16 18:03:53 +1300

    initial order form commit

--- src/views/index.js
+++ src/views/index.js
@@ -2,4 +2,5 @@ export { default as Bond } from "./Bond/Bond";
 export { default as ChooseBond } from "./ChooseBond/ChooseBond";
 export { default as Stake } from "./Stake/Stake";
 export { default as Mint } from "./Mint/Mint";
+export { default as OrderForm } from "./OrderForm";
 // export { default as TreasuryDashboard } from "./TreasuryDashboard/TreasuryDashboard";
